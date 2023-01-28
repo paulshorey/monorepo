b46b4f264fef157a3a2303d22c99ec1513dcaae0
Paul Shorey <web@artspaces.net>
2023-01-27 21:48:52 -0600

    (nlp) edit deploy script

--- apis/nlp/_bash/_start_production.sh
+++ apis/nlp/_bash/_start_production.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 ##/etc/init.d/logdna-agent start
-echo "_start_production.sh"
+
+function loglabel() {
+  if [ "$1" ]; then
+    echo -e "\033[48;5;14m\033[38;5;15m$1\033[0m"
+  fi
+}
+
+loglabel "_start_production.sh"
 cwd=$( cd "$(dirname "$0")" ; pwd -P )
 rootdir=$cwd/..
 monodir=$rootdir/../..
 cd $monodir
-echo "\$cwd = $cwd"
-echo "\$rootdir = $rootdir"
+loglabel "\$cwd = $cwd"
+loglabel "\$rootdir = $rootdir"
 
 # 
 # This script is run by /etc/crontab
 #
-echo "This script is run by /etc/crontab"
+loglabel "This script is run by /etc/crontab"
 eval "$(ssh-agent -s)"
 ssh-add /root/.ssh/2022 # fixMe: using /root instead of ~ because it's run by /etc/crontab
 
 # 
 # Reset codebase
 #
-echo "Reset codebase"
-echo "RESET root"
+loglabel "Reset codebase"
+loglabel "RESET root"
 git fetch --all
 git add .
 branch=$(git symbolic-ref --short HEAD);
@@ -27,7 +34,7 @@ git reset --hard --no-recurse-submodules HEAD # clear local changes
 git reset --hard --no-recurse-submodules origin/$branch # reset to remote
 git pull
 cd $rootdir
-echo "RESET project"
+loglabel "RESET project"
 git fetch --all
 git add .
 branch=$(git symbolic-ref --short HEAD);
@@ -40,7 +47,7 @@ source $rootdir/_bash/.aliases.sh
 #
 # Remove unused packages, to not waste time/space installing their dependencies
 #
-echo "Remove unused packages"
+loglabel "Remove unused packages"
 rm -rf $rootdir/apps
 rm -rf $rootdir/modules/ui
 find $rootdir/apis -type d -maxdepth 1 -mindepth 1 -print | grep -v "nlp" | xargs rm -rf
@@ -49,7 +56,7 @@ find $rootdir/apis -type d -maxdepth 1 -mindepth 1 -print | grep -v "nlp" | xarg
 #
 # Reset Install all monorepo dependencies
 #
-echo "Reset Install all monorepo dependencies"
+loglabel "Reset Install all monorepo dependencies"
 cd $monodir
 yarn
 cd $rootdir
@@ -57,7 +64,7 @@ cd $rootdir
 #
 # Map the port numbers
 #
-echo "Map the port numbers"
+loglabel "Map the port numbers"
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 1080
 ufw allow 80/tcp
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 1443
@@ -67,7 +74,7 @@ ufw allow 443/tcp
 # Reset cache files
 #
 # domainAvailability
-echo "Reset cache files"
+loglabel "Reset cache files"
 rm -rf $rootdir/tmp
 mkdir $rootdir/tmp
 #
@@ -79,7 +86,7 @@ mkdir $rootdir/tmp/localStorage-whois6
 #
 # Start node process
 #
-echo "Reset cache files"
+loglabel "Reset cache files"
 pm2 stop all || true
 pm2 delete all || true
 pm2 start ts-node-esm -f -- --transpile-only --esm --experimental-specifier-resolution=node -r tsconfig-paths/register $rootdir/api;
