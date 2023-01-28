6c1af68a5cad8628f07e147cd0f5026ba8be3ee2
Paul Shorey <web@artspaces.net>
2023-01-27 21:25:36 -0600

    (nlp) edit deploy script

--- apis/nlp/_bash/_start_production.sh
+++ apis/nlp/_bash/_start_production.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ##/etc/init.d/logdna-agent start
+echo "_start_production.sh"
 cwd=$( cd "$(dirname "$0")" ; pwd -P )
 rootdir=$cwd/../
 monodir=$rootdir/../../
@@ -8,12 +9,14 @@ cd $monodir
 # 
 # This script is run by /etc/crontab
 #
+echo "This script is run by /etc/crontab"
 eval "$(ssh-agent -s)"
 ssh-add /root/.ssh/2022 # fixMe: using /root instead of ~ because it's run by /etc/crontab
 
 # 
 # Reset codebase
 #
+echo "Reset codebase"
 echo "RESET root"
 git fetch --all
 git add .
@@ -33,6 +36,7 @@ git pull
 #
 # Remove unused packages, to not waste time/space installing their dependencies
 #
+echo "Remove unused packages"
 rm -rf $rootdir/apps
 rm -rf $rootdir/modules/ui
 find $rootdir/apis -type d -maxdepth 1 -mindepth 1 -print | grep -v "nlp" | xargs rm -rf
@@ -40,6 +44,7 @@ find $rootdir/apis -type d -maxdepth 1 -mindepth 1 -print | grep -v "nlp" | xarg
 #
 # Reset Install all monorepo dependencies
 #
+echo "Reset Install all monorepo dependencies"
 cd $monodir
 yarn
 cd $rootdir
@@ -47,6 +52,7 @@ cd $rootdir
 #
 # Map the port numbers
 #
+echo "Map the port numbers"
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 1080
 ufw allow 80/tcp
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 1443
@@ -56,6 +62,7 @@ ufw allow 443/tcp
 # Reset cache files
 #
 # domainAvailability
+echo "Reset cache files"
 rm -rf $rootdir/tmp/localStorage-domainsAvailability
 sleep 2
 mkdir $rootdir/tmp/localStorage-domainsAvailability
@@ -71,6 +78,7 @@ mkdir $rootdir/tmp/localStorage-whois6
 #
 # Start node process
 #
+echo "Reset cache files"
 pm2 stop all || true
 pm2 delete all || true
 pm2 start ts-node-esm -f -- --transpile-only --esm --experimental-specifier-resolution=node -r tsconfig-paths/register $rootdir/api;
